92a0035bd52b8e358bf2c25dfa57ec3314f20d22
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-31 13:13:23 -0700

    rendering list items differenly depending on bool value of is_bought

--- render-utils.js
+++ render-utils.js
@@ -2,6 +2,7 @@ export function renderListItem(item) {
     const div = document.createElement('div');
     const p = document.createElement('p');
 
+    item.is_bought ? div.classList.add('bought') : div.classList.add('notBought');
     p.textContent = `${item.quantity}X ${item.item}`;
     div.append(p);
     return div;
