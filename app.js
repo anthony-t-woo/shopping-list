5ca95aca74e2368743213a2497ad5e2b4cfab837
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-31 11:44:48 -0700

    validated that formData is assigned to an object to be fed into insert function

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
+import { checkAuth } from './fetch-utils.js';
+
+checkAuth();
 
 /* Get DOM Elements */
-
+const formEl = document.querySelector('.add-item-form');
 /* State */
-
+let listData = [];
 /* Events */
+window.addEventListener('load', async () => {});
 
+formEl.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(formEl);
+    const insert = { item: formData.get('item'), quantity: formData.get('qty') };
+    console.log(insert);
+});
 /* Display Functions */
